1357626b2616e108fc6f8a698bf1eb638d34579e
Chris M <chrismok97@gmail.com>
2022-05-19 19:44:39 -0700

    clean up calendar - need to fix

--- public/assets/scripts/calendar.js
+++ public/assets/scripts/calendar.js
@@ -9,24 +9,6 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
 
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
 function showCalendar(month, year) {
     let first_day = new Date(year, month).getDay();
     let table = document.getElementById("calendar-days");
