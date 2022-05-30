e84af2bc0e8940f9fb2207d47efcef2534fde9fe
Chris M <chrismok97@gmail.com>
2022-05-30 14:47:16 -0700

    render map correctly, inserted one marker, working on dynamic markers

--- public/assets/scripts/map.js
+++ public/assets/scripts/map.js
@@ -1,9 +1,22 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpY2tlbmJpbmdib25nIiwiYSI6ImNsM25keThmODBiNWozZHFoNjI4cnB3d24ifQ.L6FwznXuUYHof2W--CoPcg';
 
+fetch("https://vacationwiththeboys.herokuapp.com/lalv22/calendar")
+    .then(res => res.json)
+    .then(data => console.log(data))
 
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    zoom: 10
-    });
+    center: [-117.918976, 33.812511],
+    zoom: 14
+});
+
+// controls
+const controls = new mapboxgl.NavigationControl();
+map.addControl(controls, "bottom-right") 
+
+const desertpalms = new mapboxgl.Marker()
+    .setLngLat([-117.91648, 33.803744])
+    .setPopup(new mapboxgl.Popup().setHTML("<p>Desert Palms Hotel</p>"))
+    .addTo(map);
 
